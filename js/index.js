562746d0f974e2e47ef8d0760e684c350df67d41
mdhshawon69 <66409968+MDHShawon@users.noreply.github.com>
2020-09-30 19:05:41 +0600

    project2
    This is my another project

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeOut:100,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
